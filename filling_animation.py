dbdb1c4d7714664a0053b0f0933d9139237fb4f5
JentGent <62735169+JentGent@users.noreply.github.com>
2024-08-24 19:37:37 -0700

    update animation

--- filling_animation.py
+++ filling_animation.py
@@ -28,12 +28,6 @@ MAX_INJECTION_PRESSURE = 20
 FOLDER = f"temp/filling_injection/{HYPOTENSION}_{CVP}_{CARDIAC_PHASE}_{MAX_INJECTION_PRESSURE}_{INJECTION_LOCATION}_{OCCLUDED}"
 
 def main():
-    
-    if os.path.exists(FOLDER):
-        raise FileExistsError(f"Folder already exists at {FOLDER}")
-    else:
-        os.makedirs(FOLDER)
-        print(f"Folder created at {FOLDER}")
 
     avm.PREDEFINED_RESISTANCE = False
     start_time = time.time()
@@ -43,28 +37,37 @@ def main():
 
     pressure_sets = [injections.generate_pressure_set(INJECTION_LOCATION if injection_pressure else None, injection_pressure, HYPOTENSION, CVP, CARDIAC_PHASE) for injection_pressure in range(0, MAX_INJECTION_PRESSURE + 1)]
 
-    node_pos = copy.deepcopy(avm.NODE_POS_TEMPLATE)
-    num_compartments = generate.normint(3, 6, sd=1)
-    num_columns = generate.normint(3, 7, sd=1)
-    num_compartments = 6
-    num_columns = 7
-    num_intercompartmental_vessels = num_compartments * num_columns * 2
-    print(f"{num_compartments} compartments, {num_columns} columns")
-    
-    network = avm.edges_to_graph(avm.VESSELS_TEMPLATE)
-    network, _ = generate.compartments(network, feeders, drainers, FIRST_INTRANIDAL_NODE_ID, node_pos, num_compartments, num_columns, num_intercompartmental_vessels, fistula_start = "AF2", fistula_end = "DV2")
+    while True:
+        node_pos = avm.NODE_POS_TEMPLATE
+        num_compartments = generate.normint(3, 6, sd=1)
+        num_columns = generate.normint(3, 7, sd=1)
+        num_compartments = 6
+        num_columns = 7
+        num_intercompartmental_vessels = num_compartments * num_columns * 2
+        print(f"{num_compartments} compartments, {num_columns} columns")
+        
+        network = avm.edges_to_graph(avm.VESSELS_TEMPLATE)
+        network, _ = generate.compartments(network, feeders, drainers, FIRST_INTRANIDAL_NODE_ID, node_pos, num_compartments, num_columns, num_intercompartmental_vessels, fistula_start = "AF2", fistula_end = "DV2")
+        occluded = [edge for edge in network.edges(data=True) if edge[1] == OCCLUDED][0] if OCCLUDED else None
+        if occluded: network.remove_edge(occluded[0], occluded[1])
 
-    flows, pressures, all_edges, graphs, *error = avm.simulate_batch(network, "SP", 0, pressure_sets, CALCULATE_ERROR)
-    max_jump, max_fill, prev = 0, 0, 0
-    for graph in graphs:
-        new = avm.get_stats(graph, graphs[0], 6, 0, "DV3")["Percent filled post-injection (%)"]
-        max_fill = max(new, max_fill)
-        if new - prev > max_jump:
-            max_jump = new - prev
-        prev = new
-    print(max_jump, max_fill)
+        flows, pressures, all_edges, graphs, *error = avm.simulate_batch(network, "SP", 0, pressure_sets, CALCULATE_ERROR)
+        max_jump, max_fill, prev = 0, 0, 0
+        for injection_pressure, graph in enumerate(graphs):
+            new = avm.get_stats(graph, graphs[0], 4, injection_pressure, INJECTION_LOCATION)["Percent filled post-injection (%)"]
+            max_fill = max(new, max_fill)
+            max_jump = max(new - prev, max_jump)
+            prev = new
+        print(max_jump, max_fill)
+        if max_jump < 27 and max_fill > 70: break
     print("generated")
     error = error if error else None
+    
+    if os.path.exists(FOLDER):
+        raise FileExistsError(f"Folder already exists at {FOLDER}")
+    else:
+        os.makedirs(FOLDER)
+        print(f"Folder created at {FOLDER}")
 
     animation_phase = 0
     filled_vessels = set()
@@ -76,6 +79,9 @@ def main():
         flow = flows[:, injection_pressure]
         pressure = pressures[:, injection_pressure]
         graph = graphs[injection_pressure]
+
+        if occluded:
+            graph.add_edge(occluded[0], occluded[1], occluded=True, pressure=0, **occluded[2])
 
         vessel_count = 0
         for start, end, type in graph.edges.data("type"):
@@ -100,7 +106,7 @@ def main():
                 for vessel in filled_vessels:
                     if vessel not in graph.edges: vessel = (vessel[1], vessel[0])
                     graph.edges[vessel]["reached"] = True
-                figures.display_filling(graph, node_pos)
+                figures.display_filling(avm.get_nidus(graph, True), node_pos)
                 # plt.subplots_adjust(left=0, right=1, bottom=0, top=1)
 
                 plt.text(0.01, 0.99, f"Injection pressure: {int(injection_pressure)} mmHg\nFilling: {int(len(filled_vessels) / vessel_count * 100)}%", transform=plt.gca().transAxes, fontsize=15, verticalalignment='top')
